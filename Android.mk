a9573fb4c1f2b90fe1ce3ee121810cb47f059d0a
James Christopher Adduono <jc@adduono.com>
2016-12-10 22:16:07 -0500

    Set up for Android build environment inclusion
    Change-Id: If7ffc7f1eac39645f7f9a0041390891eabb4de5b

---
+++ Android.mk
@@ -0,0 +1,43 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sepolicy-inject
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libc libcutils libselinux libsepol
+LOCAL_SRC_FILES := sepolicy-inject.c
+LOCAL_C_INCLUDES := external/libselinux/include external/selinux/libsepol/include
+LOCAL_CFLAGS += -std=gnu11
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sepolicy-inject
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libselinux libsepol
+LOCAL_SRC_FILES := sepolicy-inject.c
+LOCAL_C_INCLUDES := external/libselinux/include external/selinux/libsepol/include
+LOCAL_CFLAGS += -std=gnu11
+include $(BUILD_HOST_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sepolicy-inject-N
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libc libcutils libselinux libsepol-N
+LOCAL_SRC_FILES := sepolicy-inject.c
+LOCAL_C_INCLUDES := external/libselinux/include $(LOCAL_PATH)/libsepol-N/include
+LOCAL_CFLAGS += -std=gnu11
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := sepolicy-inject-N
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libselinux libsepol-N
+LOCAL_SRC_FILES := sepolicy-inject.c
+LOCAL_C_INCLUDES := external/libselinux/include $(LOCAL_PATH)/libsepol-N/include
+LOCAL_CFLAGS += -std=gnu11
+include $(BUILD_HOST_EXECUTABLE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
