2b788c5e41852a2f88475548faab9bb44aa84540
James Christopher Adduono <jc@adduono.com>
2017-01-09 21:20:17 -0500

    Build shared for this branch
    Change-Id: I902122230a552d4d643f8cd0e1d0981942afeb38

--- Android.mk
+++ Android.mk
@@ -3,8 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := sepolicy-inject
 LOCAL_MODULE_TAGS := optional
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := libc libcutils libselinux libsepol
+#LOCAL_FORCE_STATIC_EXECUTABLE := true
+#LOCAL_STATIC_LIBRARIES := libc libcutils libselinux libsepol-N
+LOCAL_STATIC_LIBRARIES := libselinux libsepol
 LOCAL_SRC_FILES := sepolicy-inject.c
 LOCAL_C_INCLUDES := external/libselinux/include external/selinux/libsepol/include
 LOCAL_CFLAGS += -std=gnu89 -Os
@@ -23,8 +24,9 @@ include $(BUILD_HOST_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_MODULE := sepolicy-inject-N
 LOCAL_MODULE_TAGS := optional
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := libc libcutils libselinux libsepol-N
+#LOCAL_FORCE_STATIC_EXECUTABLE := true
+#LOCAL_STATIC_LIBRARIES := libc libcutils libselinux libsepol-N
+LOCAL_STATIC_LIBRARIES := libselinux libsepol-N
 LOCAL_SRC_FILES := sepolicy-inject.c
 LOCAL_C_INCLUDES := external/libselinux/include $(LOCAL_PATH)/libsepol-N/include
 LOCAL_CFLAGS += -std=gnu89 -Os
